9c8331735fb951d1f0016220a8fd45dc5be9ff2e
gsnbng <sheshanarayanag@gmail.com>
2016-08-19 18:08:50 +0530

    Changes made 18-08-2016

---
+++ dms/dms/doctype/doc_chng_master/doc_chng_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, xyz and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Doc_chng_master', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ dms/dms/doctype/item_doc_master/item_doc_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, xyz and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Item_doc_master', {
+	refresh: function(frm) {
+
+	}
+});
