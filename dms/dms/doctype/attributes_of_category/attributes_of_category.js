8fed02350893882d9941ad26f124a46094efc055
Sheshanarayana G <sheshanarayanag@gmail.com>
2022-07-20 20:46:35 +0530

    Changes made

---
+++ dms/dms/doctype/attributes_of_category/attributes_of_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, xyz and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Attributes of Category', {
+	// refresh: function(frm) {
+
+	// }
+});
