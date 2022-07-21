73f157d5acd71ac368bf287e2c066a564f695330
Sheshanarayana G <sheshanarayanag@gmail.com>
2022-07-21 15:12:34 +0530

    Changes made

--- dms/dms/doctype/item_dms/item_dms.js
+++ dms/dms/doctype/item_dms/item_dms.js
@@ -3,23 +3,42 @@
 
 frappe.ui.form.on('Item dms', {
 
-  refresh: function(frm){
+  cat_name: function(frm){
 
   let cat_name = frm.doc.cat_name;
 
-   if(cat_name){
+//    frm.doc.cat_name=;
+
+//    frm.add_child('item_character',{attribute: 'Shape'});
+//    frm.refresh_field('item_character')
     frappe.call({
      method: "dms.dms.doctype.item_dms.item_dms.get_attribute_category",
-     args: {cat_name: cat_name}
-    }).then(records => {
+     args: {'cat_name':cat_name},
+     callback: function(r){
+    //   frm.doc.item_character = []
+//   msgprint('not passing records'+r.message+cat_name)   
+   $.each(r.message, function(i, r){
+       frm.add_child('item_character',{attribute: r.attribute,attribute_of:cat_name});
+       frm.refresh_field('item_character')
+//       msgprint('not passing records'+r.attribute+cat_name)   
 
-   frm.doc.item_character = []
-
-   $.each(records, function(i, r){
-       frm.add_child("item_character",{attribute: r.attribute});
-   })
+    }) 
+   },
  })
 
-}
   }
+
+//   refresh(frm) {
+//           frm.set_query('attribute_value','item_character',
+//                                   function(doc,cdt,cdn) {
+
+//                       var d = locals[cdt][cdn];
+//                        return{
+//                              " filters": {
+//                                     "attribute" : d.attribute
+//                                       }
+//                              };
+//                           });
+//                }
+
 });
