b990746c7c499e92cc5443bb9de2addff725ca4c
Sheshanarayana G <sheshanarayanag@gmail.com>
2022-07-21 04:42:46 +0000

    Update dms/dms/doctype/item_dms/item_dms.js

--- dms/dms/doctype/item_dms/item_dms.js
+++ dms/dms/doctype/item_dms/item_dms.js
@@ -2,7 +2,28 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Item dms', {
-	refresh: function(frm) {
 
-	}
+  author: function(frm){
+
+  let cat_name = frm.doc.cat_name;
+
+   if(author){
+    frappe.call({
+     method: "dms.dms.doctype.item_dms.item_dms.get_attribute_category",
+     args: {cat_name: cat_name}
+    }).done((r) => {
+
+   frm.doc.my_article = []
+
+   $.each(r.message, function(_i, e){
+   let entry = frm.add_child("item_character");
+   entry.article_name = e.name;
+ })
+ refresh_field("item_character")
+
+  })
+ }
+
+}
+
 });
