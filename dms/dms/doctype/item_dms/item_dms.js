ceacf119b020a156c1ab22f388136d26f4f2602d
Sheshanarayana G <sheshanarayanag@gmail.com>
2022-08-05 15:27:58 +0000

    Update dms/dms/doctype/item_dms/item_dms.js

--- dms/dms/doctype/item_dms/item_dms.js
+++ dms/dms/doctype/item_dms/item_dms.js
@@ -3,138 +3,105 @@
 
 frappe.ui.form.on('Item dms', {
 
-  cat_name: function(frm){
+                      cat_name: function(frm){
 
-  let cat_name = frm.doc.cat_name;
+                                let cat_name = frm.doc.cat_name;
 
-//    frm.doc.cat_name=;
+                                frappe.call({
+                                       method: "dms.dms.doctype.item_dms.item_dms.get_attribute_category",
+                                         args: {'cat_name':cat_name},
+                                     callback: function(r){
+   
+                                          $.each(r.message, function(i, r){
+                                          frm.add_child('item_character',{attribute: r.attribute,attribute_of:cat_name});
+                                          frm.refresh_field('item_character')
+  
+                                             }) 
+                                            },
+                                        });
 
-//    frm.add_child('item_character',{attribute: 'Shape'});
-//    frm.refresh_field('item_character')
-    frappe.call({
-     method: "dms.dms.doctype.item_dms.item_dms.get_attribute_category",
-     args: {'cat_name':cat_name},
-     callback: function(r){
-    //   frm.doc.item_character = []
-//   msgprint('not passing records'+r.message+cat_name)   
-        $.each(r.message, function(i, r){
-       frm.add_child('item_character',{attribute: r.attribute,attribute_of:cat_name});
-       frm.refresh_field('item_character')
-//       msgprint('not passing records'+r.attribute+cat_name)   
+                                   },
 
-    }) 
-   },
- });
-
-  },
-
-   refresh: function(frm) {
-           frm.set_query('attribute_value','item_character',
+                      refresh: function(frm) {
+                                frm.set_query('attribute_value','item_character',
                                    function(doc,cdt,cdn) {
 
-                       var d = locals[cdt][cdn];
-                        return{
-                              query: 'dms.dms.doctype.item_dms.item_dms.query_attribute',
-                              filters : {
-                                     'attribute' : d.attribute
-                                       }
+                                     var d = locals[cdt][cdn];
+                                     return{
+                                             query: 'dms.dms.doctype.item_dms.item_dms.query_attribute',
+                                          filters : {
+                                                     'attribute' : d.attribute
+                                                  }
                                                             
-                            }
-                           });
-                },
+                                        }
+                                      });
+                                  },
 
-    before_save: function(frm) {
-		 //let row = frappe.get_doc(cdt,cdn);
-        //var row = locals['Item character attribute'][cdn];
-        //frappe.model.set_value(d.doctype, d.name, 'attribute_value_both', (d.attribute_value));
-        if(frm.doc.cat_name){
-        frm.doc.item_name = frm.doc.cat_name;
-        $.each(frm.doc.item_character,function(i,  d) {
-         frm.doc.item_name += ' '+ d.attribute_value_both;
-        //msgprint("Message6" +frm.doc.item_name);
-    });
-           }
-        //msgprint("Message5" +frm.doc.item_name);
-    
-	},
+                   before_save: function(frm) {
+		                                           if(frm.doc.cat_name){
+                                                        frm.doc.item_name = frm.doc.cat_name;
+                                                        $.each(frm.doc.item_character,function(i,  d) {
+                                                        frm.doc.item_name += ' '+ d.attribute_value_both;
+                                                        });
+                                                     }
+            
+	                                },
 
-  refresh: function(frm,cdt,cdn) {
-		 if (!frm.doc.trf_prodn && !frm.doc.__islocal) {
-                 frm.add_custom_button(__("Transfer Item to Prodn"), function() {
-		//	frappe.set_route("List", "Item Price", {"item_code": frm.doc.name});
-                 var itemx = frappe.model.get_doc(cdt,cdn);
-                 msgprint("M" + itemx.item_name);
-                 frappe.call({
-//                              url: "api/resource/Item/",
-                                method: "frappe.client.get_value",
-//                                doctype: "Item",
-//                              type: "get",
-//                              async: false,
-                              args: {
-                                    doctype: "Item",
-                                     filters: [["item_name","=",itemx.item_name]],
-                                     fieldname: ["item_name","item_code"]
-                                       },
+                       refresh: function(frm,cdt,cdn) {
+		                                          if (!frm.doc.trf_prodn && !frm.doc.__islocal) {
+                                                  frm.add_custom_button(__("Transfer Item to Prodn"), function() {
+		                                              var itemx = frappe.model.get_doc(cdt,cdn);
+                                                  msgprint("M" + itemx.item_name);
+                                                 frappe.call({
+                                                      method: "frappe.client.get_value",
+                                                        args: {
+                                                                 doctype: "Item",
+                                                                 filters: [["item_name","=",itemx.item_name]],
+                                                               fieldname: ["item_name","item_code"]
+                                                           },
 
-//                                    },
+                                                      callback: function(r) {
+                                                                msgprint("Message_1");
+                                                                console.log(r.message);
+                                                                msgprint(r.message.item_name);
+                                                                msgprint(r.message.item_code);
 
-                              callback: function(r) {
-                            msgprint("Message_1");
-                             console.log(r.message);
-                              msgprint(r.message.item_name);
-                              msgprint(r.message.item_code);
+                                                                if (!r.message.item_name){
+                      
+                                                                         frappe.call({
+                                                                            url: "/api/resource/Item/",
+                                                                           type: "post",
+                                                                           args: {
+                                                                             data: {"item_name" : itemx.item_name,
+                                                                                           "uom": itemx.uom,
+                                                                                    "item_group":itemx.item_group,
+                                                                                   "description":itemx.description,
+                                                                                  "manufacturer":itemx.manufacturer,
+                                                                        "manufacturer_part_no":itemx.manufacturer_part_no
+                                                                                }
+                                                                             },
+                                                                          callback: function(r1) {
+                                                                                   console.log(JSON.stringfy(r1));
+                                                                                   msgprint(r1.message.item_code);
+                                                                                         }
+                                                                                     });
+                                                                           frm.set_value({trf_prodn : 1});
+                                                                         frm.set_value({item_code : r.message.item_code});
+                                                                          frm.save();
+                                                                              }
+                                                                          }
+                                                                     });
+                                                        msgprint(frm.doc.trf_prodn);
 
-                             if (!r.message.item_name){
-                                    
-                              
-                   frappe.call({
-                               url: "/api/resource/Item/",
-                               type: "post",
-                               args: {
-                                   data: {"item_name" : itemx.item_name,
-                                                 "uom": itemx.uom,
-                                          "item_group":itemx.item_group,
-                                           "description":itemx.description,
-                                          "manufacturer":itemx.manufacturer,
-                                  "manufacturer_part_no":itemx.manufacturer_part_no
-                                       }
-
-                                    },
-                              callback: function(r1) {
-                                console.log(JSON.stringfy(r1));
-//                              $.each(r.message, function(i, r){
-                               msgprint(r1.message.item_code);
-
-
-//                                        })    
-                                  }
-                            });
-                               frm.set_value({trf_prodn : 1});
-                               frm.set_value({item_code : r.message.item_code});
-                               frm.save();
-
-
-                               }
-                         }
-                   });
-                 msgprint(frm.doc.trf_prodn);
-
-		});
-           }
-	}
-
-});
-
+		                                                            });
+                                                            }    
+	                                                    }
+                                });
 
 frappe.ui.form.on('Item character attribute', {
-	attribute_value:function(frm,cdt,cdn){
-        
-       let row = frappe.get_doc(cdt,cdn);
-       //msgprint("message"+row.attribute_value);
-       row.attribute_value_both = row.attribute_value
-       //msgprint(row);
-       frm.set_df_property('attribute_value_both',  'read_only',  !frm.doc.attribute_value ? 0 : 1);
-       
-        } 
-	
-});
+	                            attribute_value:function(frm,cdt,cdn){
+                             let row = frappe.get_doc(cdt,cdn);
+                            row.attribute_value_both = row.attribute_value
+                           frm.set_df_property('attribute_value_both',  'read_only',  !frm.doc.attribute_value ? 0 : 1);
+                                                             }                 
+	                                         });
